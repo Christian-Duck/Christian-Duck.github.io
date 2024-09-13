9d526c8745fdbb556206d4935b13eed4b74e26f1
Christian-Duck <celipps25@gmail.com>
2024-09-13 17:06:35 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -34,31 +34,38 @@ $(function () {
     //////////ONLY CHANGE BELOW THIS POINT///////////
     /////////////////////////////////////////////////
 
+    //note world size is 1400 x 750
+
     // TODO 1
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+    createPlatform(700, 200, 200, 20);
+    createPlatform(500, 350, 200, 20);
+    createPlatform(300, 400, 200, 20);
+    createPlatform(700, 400, 200, 20)
+    createPlatform(900, 500, 200, 20)
 
-
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
+    createCollectable("steve", 600, 500, 20, 0.5);
+    createCollectable("grace", 500, 250);
+    createCollectable("diamond",700, 100)
 
-
-    
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
-
+    createCannon("left", 200, 4000,)
+    createCannon("right", 200, 4000,)
+    createCannon("top", 700, 2000,)
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
